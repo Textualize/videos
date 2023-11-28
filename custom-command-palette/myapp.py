4333109f4472dfc9624b95400c95b3f53838feb2
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-11-28 13:55:45 +0000

    Add video about custom command palette.

---
+++ custom-command-palette/myapp.py
@@ -0,0 +1,45 @@
+# Written with Textual 0.42.0.
+
+from functools import partial
+
+from textual.app import App
+from textual.command import Hit, Hits, Provider
+from textual.widgets import Static
+
+
+COLOURS = {
+    "red",
+    "green",
+    "blue",
+    "white",
+    "black",
+}
+
+
+class ColourCommandProvider(Provider):
+    async def search(self, query: str) -> Hits:
+        matcher = self.matcher(query)
+
+        for colour in COLOURS:
+            score = matcher.match(colour)
+            if score > 0:
+                yield Hit(
+                    score,
+                    matcher.highlight(colour),
+                    partial(self.app.add_strip, colour),
+                    help=f"Create a {colour} strip.",
+                )
+
+
+class MyApp(App[None]):
+    COMMANDS = App.COMMANDS | {ColourCommandProvider}
+
+    def add_strip(self, colour: str) -> None:
+        static = Static()
+        static.styles.background = colour
+        self.mount(static)
+
+
+app = MyApp()
+if __name__ == "__main__":
+    app.run()
