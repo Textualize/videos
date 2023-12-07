45dde857db22942b0c850790bf6d7bbbefcaf818
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-12-07 13:11:08 +0000

    Countdown timer idea.

---
+++ countdown-timer/myapp.py
@@ -0,0 +1,35 @@
+"""
+Show how to create a basic countdown timer with `Digits`, `set_interval`, and reactives.
+"""
+
+from textual.app import App, ComposeResult
+from textual.containers import Center, Middle
+from textual.reactive import reactive
+from textual.widgets import Digits
+
+
+class CountdownApp(App[None]):
+    CSS = """Digits { width: auto; }"""
+    time_left = reactive(15, init=False)
+
+    def compose(self) -> ComposeResult:
+        with Center():
+            with Middle():
+                yield Digits()
+
+    def tick(self) -> None:
+        self.time_left -= 1
+        if not self.time_left:
+            self.tick_timer.stop()
+
+    def watch_time_left(self, time_left: int) -> None:
+        time, seconds = divmod(time_left, 60)
+        hours, minutes = divmod(time, 60)
+        self.query_one(Digits).update(f"{hours:02}:{minutes:02}:{seconds:02}")
+
+    def on_mount(self) -> None:
+        self.tick_timer = self.set_interval(1, self.tick)
+
+
+if __name__ == "__main__":
+    CountdownApp().run()
